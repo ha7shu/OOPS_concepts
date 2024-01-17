342b9f0319cedbe46a1c4e6c16e824960fa2a085
Harsh Sharma <harshusharma9896@gmail.com>
2024-01-17 23:08:18 +0530

    Update project structure

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    cout << "A basic C++ program" << endl;
-    cout << "Compile and run this program to check if everything is working well." << endl;
-    return 0;
-}
